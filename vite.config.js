15d763fd70fd2927e0092fb02145507652ae6521
AhmAd-bAkr1 <msd39777@gmail.com>
2023-08-07 23:38:22 +0300

    upload New Data To Repo

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: "/Stakepool-Frontend/",
+    plugins: [react()],
+    build: {
+        chunkSizeWarningLimit: 1600,
+    },
+});
