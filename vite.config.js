6ffd2850667fc89971f48cea8950f52f8343a19f
AhmAd-bAkr1 <msd39777@gmail.com>
2023-08-08 00:09:28 +0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/Stakepool-Frontend/",
     plugins: [react()],
     build: {
         chunkSizeWarningLimit: 1600,
